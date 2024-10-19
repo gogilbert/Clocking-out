6f77986917b14103f1bd823eaeb723889da0fb97
John Boersema <jsboersema@hotmail.com>
2024-10-19 17:47:24 -0400

    Intro cutscene

--- scripts/enemy_pathing.gd
+++ scripts/enemy_pathing.gd
@@ -3,6 +3,6 @@ extends PathFollow2D
 var speed = 0.01
 
 func _process(delta):
-	if !Globalscript.enemy_chasing:
+	if !Globalscript.enemy_chasing && Globalscript.currentState >  4:
 		progress_ratio += delta * speed
 		Globalscript.enemy_prevPosition = position
--- scripts/storeFloor.gd
+++ scripts/storeFloor.gd
@@ -2,11 +2,21 @@ extends Node2D
 
 @onready var doorL: InteractionArea = $DoorLInteract
 @onready var doorR: InteractionArea = $DoorRInteract
+@onready var frontI: InteractionArea = $FrontInteract
+
+const frontText: Array[String] = [
+	"Oh no! The door is locked!"
+]
+
+const enemyText: Array[String] = [
+	"Who's there?"
+]
 
 func _ready():
 	doorL.interact = Callable(self, "_on_interact_doorL")
 	doorR.interact = Callable(self, "_on_interact_doorR")
-
+	frontI.interact = Callable(self, "_on_interact_front")
+	
 	match Globalscript.from_scene:
 		"doorL":
 			$Player.position.x = 319
@@ -17,6 +27,18 @@ func _ready():
 
 func _process(delta):
 	change_scene()
+	if !DialogManager.is_dialog_active && Globalscript.currentState == 3:
+		Globalscript.currentState = 4
+		$Player/Camera2D.offset = Vector2(-600, -50)
+		DialogManager.start_dialog($EnemyPath/PathFollow2D/Enemy.global_position, enemyText)
+	if !DialogManager.is_dialog_active && Globalscript.currentState == 4:
+		Globalscript.currentState = 5
+		$Player/Camera2D.offset = Vector2(0, 0)
+	
+func _on_interact_front():
+	$FrontInteract/CollisionShape2D.disabled = true
+	Globalscript.currentState = 3
+	DialogManager.start_dialog($Player.position, frontText)
 
 func _on_interact_doorL():
 	Globalscript.transition_scene = true
